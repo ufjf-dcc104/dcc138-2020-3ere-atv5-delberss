80201d649c21da1b0a77dfeee74add3557b82d64
Delber Soares <11241419604@estudante.ufjf.br>
2021-02-23 15:25:14 -0300

    Adiciona exemplo de uso de audio basico

--- js/Main.js
+++ js/Main.js
@@ -8,6 +8,7 @@ assets.carregaImagem("garota", "assets/garota.png");
 assets.carregaImagem("esqueleto", "assets/skelly.png");
 assets.carregaImagem("orc", "assets/orc.png");
 assets.carregaAudio("moeda", "assets/coin.wav");
+assets.carregaAudio("boom", "assets/boom.wav");
 
 
 const canvas = document.querySelector("canvas");
@@ -32,6 +33,9 @@ document.addEventListener("keydown",  (e)=>{
             break;
         case "c":
             assets.audio("moeda").play();
+            break;
+        case "b":
+            assets.audio("boom").play();
             break;
     }
 })
