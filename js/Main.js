4428427f31985b270cf0fed34269800fefb89c9f
Delber Soares <11241419604@estudante.ufjf.br>
2021-02-23 14:45:27 -0300

    Cria arquivo de gerenciador de recursos

--- js/Main.js
+++ js/Main.js
@@ -1,9 +1,23 @@
 import Cena from "./Cena.js"
 import Sprite from "./Sprite.js";
+import AssetManager from "./AssetManager.js";
+
+const img1 = new Image();
+img1.src="assets/garota.png";
+const img2 = new Image();
+img2.src="assets/skelly.png";
+const img3 = new Image();
+img3.src="assets/orc.png";
+
+document.body.appendChild(img1);
+document.body.appendChild(img2);
+document.body.appendChild(img3);
+
+const assets = new AssetManager();
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const cena1 = new Cena(canvas);
+const cena1 = new Cena(canvas,assets);
 const pc = new Sprite({vx: 10});
 const en1 = new Sprite({x: 140, w: 30, color:"red"});
 
