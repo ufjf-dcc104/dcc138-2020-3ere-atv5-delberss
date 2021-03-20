43656a6ab88fd83f705e3d7564d5cfb167f2cc91
Delber Soares <11241419604@estudante.ufjf.br>
2021-03-20 20:25:22 -0300

    Adiciona perseguição nos inimigos

--- js/Main.js
+++ js/Main.js
@@ -63,13 +63,21 @@ pc.controlar = function(dt){
       }
   }
 
-const en1 = new Sprite({x: 160, vx: - 10, color:"red"});
+  
+  cena1.adicionar(pc);
+  
+  function perseguePC(dt){
+      this.vx = 25*Math.sign(pc.x - this.x);
+      this.vy = 25*Math.sign(pc.y - this.y);
+    }
+    
+const en1 = new Sprite({x: 260, color:"red", controlar: perseguePC});
+en1.controlar = perseguePC;
 
-cena1.adicionar(pc);
 cena1.adicionar(en1);
-cena1.adicionar(new Sprite({x: 115, y: 70, vy:10,  color:"red"}));
-cena1.adicionar(new Sprite({x: 115, y: 160, vy:-10,  color:"red"}));
-cena1.adicionaSprites(10); // Adiciona função para Sprites aleatorios.
+cena1.adicionar(new Sprite({x: 115, y: 70, vy:10,  color:"red", controlar: perseguePC}));
+cena1.adicionar(new Sprite({x: 115, y: 160, vy:-10,  color:"red",controlar: perseguePC}));
+//cena1.adicionaSprites(10); // Adiciona função para Sprites aleatorios.
 cena1.spriteNoIntervalo(4000); // Função que cria sprites a cada intervalo de 4segs;
 
 cena1.iniciar();
