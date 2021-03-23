8914f01b499a5906c6871c97f94aa729cff1dd52
Delber Soares <11241419604@estudante.ufjf.br>
2021-03-23 02:14:39 -0300

    Altera tela inicial com orientação para vencer

--- js/CenaCarregando.js
+++ js/CenaCarregando.js
@@ -6,19 +6,32 @@ export default class CenaCarregando extends Cena {
         this.ctx.fillStyle = "black";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.font = "20px Impact";
-        this.ctx.fillStyle = "yellow";
         this.ctx.TextAlign = "center";
-        this.ctx.fillText(
-          this.assets?.progresso(),
-          this.canvas.width / 2.5,
-          this.canvas.height / 2
-        );
-    
-        if (this.assets.acabou()) {
+
+
+          if (this.assets.acabou()) {
+            this.ctx.fillStyle = "yellow";
+            this.ctx.fillText(
+              this.assets?.progresso(),
+              this.canvas.width / 2.5,
+              this.canvas.height / 2
+            );
+          }
+          
+          if (this.assets.acabou()) {
+          this.ctx.fillStyle = "yellow";
           this.ctx.fillText(
             "Aperte espaço para continuar",
             this.canvas.width / 5,
             this.canvas.height / 2 + 40
+          );
+        }
+        if (this.assets.acabou()) {
+          this.ctx.fillStyle = "green";
+          this.ctx.fillText(
+            "Colete 13 moedas para vencer",
+            this.canvas.width / 5,
+            this.canvas.height / 2 + 80
           );
         }
     }
